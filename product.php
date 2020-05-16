3c74517b53bc6c6e5e8f381b874df0ebba330363
mashkovtseviv <mashkovtseviv@gmail.com>
2020-05-16 11:17:08 +0200

    Add php-files from 2 chapter PHP-learning html_academy

---
+++ product.php
@@ -0,0 +1,27 @@
+﻿<?php
+require('products_db.php');
+require('components/header.php');
+
+$id = $_GET['product_id'];
+$title = get_product_title($id);
+$img_url = get_img_url($id);
+$price = 10000;
+$discount = 5000;
+$is_new = true;
+$is_last = true;
+$product_class = 'item';
+
+if ($discount > 0) {
+  $price_with_discount = $price - $discount ;
+}
+
+if ($is_new) {
+  $product_class = $product_class . ' item-new';
+}
+
+if ($discount > 1400 || $is_last) {
+  $product_class = $product_class . ' item-hot';
+};
+
+require('components/product_info.php');
+require('components/footer.php');
